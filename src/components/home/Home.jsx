ee0f26a304b883aaf33822e105198e29171af963
Katie <kmunoz17@my.gcu.edu>
2023-03-03 11:06:33 -0700

    css

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
           <h2 className="i-intro"> Hello, My name is</h2>
           <div className="App">
 
-          </div>
+ 
           <h1 className="i-name"> Katie Munoz</h1>
           <div className="i-title">
             <div className="i-title-wrapper">
@@ -28,6 +28,7 @@ function Home() {
               <div className="i-title-item">Front End Developer</div>
               <div className="i-title-item">Software Developer</div>
             </div>
+          </div>
           </div>
 
           <p className="i-desc">
@@ -38,7 +39,6 @@ function Home() {
                 typewriter
 
                   .typeString("Welcome to my software developer portfolio website! My goal as a developer is to create innovative and user-friendly software that not only meets but exceeds the expectations of my clients and users. ")
-
                   .pauseFor(1000)
                   .deleteAll()
                   .typeString("I hope that this portfolio will give you a glimpse into my passion for software development and the value I can bring to your project. If you have any questions or are interested in working together, please don't hesitate to get in touch. Thank you for visiting my portfolio website!")
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -13,42 +13,50 @@ import Link from "../../img/Resume.pdf";
 function Portfolio() {
   return (
     <div className="c">
-            <div className="e-bg"></div>
+      <div className="e-bg"></div>
       <div className="d-bg"></div>
       <div className="c-wrapper">
-        <div className="c-left">
+      <div className="c-left">
           <a id="Portfolio"></a>
 
-          <h1 className="c-titles">Katie Munoz</h1>
-          <div className="d-right">
+<div className="titlec" >
+        Katie Munoz
+</div>
+    
           <form action={Link} download="resume.pdf">
          <button type="submit">Download Resume</button>
          </form>
-         </div>
          <br/>
-          <div className="d-info">
-            <div className="d-info-item">
-              <p className="d-left">
-                <b><h3>Contact</h3></b>
-               <h5>Phone Number:</h5> 623-238-2542 
-                <br/>
-                <h5>Email:</h5> kxtemas01@gmail.com
-              
+         <div class="lc" data-title="Work Experience">
+            <br/>
+            <div class="bold">
+              MEMBER SALES REPRESENTATIVE, Surprise, AZ <br/>
+              United PF, LLC, April 2021-Present 
+             
+              </div>
               <br/>
-                <a href="https://www.linkedin.com/in/katiemunoz01" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/linkedin.svg" width="32" height="32" /></a>
-              </p>
-        
+              Followed company procedures to count and manage funds received from customers.
+              Offered best-in-class service to members through prompt, courteous and professional interactions. Provided excellent customer service by assisting, responding to and resolving customer inquiries. Trained staff on office procedures and software applications, driving operations.
+              Assisted with new hire orientation and employee training.
+              Troubleshot minor problems and reported larger technical issues.
+              <br/>
+              <br/>
+              <div class="bold">
+              SHIFT MANAGER, Surprise, AZ <br/>
+              AGK Restaurants, February 2019-March 2021<br/>
+              
+              </div>
+              <br/>
+              Completed incident reports needed to document damage, theft or personal injury claims.
+              Tracked store inventories and replenished item levels by ordering new products.
+              Tracked employee progress and implemented corrective actions to minimize mistakes and promote growth. Created staff teams and delegated work tasks to increase productivity.
+              Promoted customer satisfaction by addressing complaints and issuing refunds.
+              Set employee schedules and made staffing adjustments to get shifts covered.
+              Balanced cash drawers and resolved financial discrepancies using receipts and financial document
+              <br/>
             </div>
-          </div>
-          <p className="c-descs">
-            <b>Career Objectives</b>
-            <p></p>
-            Katie is a Senior at Grand Canyon University working towards a Bachelor's Degree in Software Development. She is a dedicated student with a passion for growing her knowledge in the software development field. This field of study has provided greater experience in Object Oriented Programming and Java, as well as extensive experience with PHP and C# programming languages. Within these languages, Katie has experience with HTML, CSS, and JavaScript, particularly AJAX, for front end development, MVC design patterns within the Laravel and ASP.NET Core frameworks, and MySQL database design and manipulation. She is a quick learner with a passion for creating quality work.
-          </p>
-          <p></p>
-          <b>Skills</b>
-          <p></p>
-          <p align="left">
+         <div class="rc" data-title="Skills & Frameworks">
+          <br/>
             <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg" width="36" height="36" alt="JavaScript" /></a>
             <a href="https://www.php.net/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/php-colored.svg" width="36" height="36" alt="PHP" /></a>
             <a href="https://docs.microsoft.com/en-us/dotnet/csharp/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/csharp-colored.svg" width="36" height="36" alt="C#" /></a>
@@ -58,51 +66,60 @@ function Portfolio() {
             <a href="https://angular.io/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/angularjs-colored.svg" width="36" height="36" alt="Angular" /></a>
             <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mongodb-colored.svg" width="36" height="36" alt="MongoDB" /></a>
             <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mysql-colored.svg" width="36" height="36" alt="MySQL" /></a>
-          </p>
+        
+          </div>
+      
+        
 
+          <div class="rc" data-title="Contact">
+                <br/>
+             <h6>Phone Number: </h6>
+             <h7> 623-238-2542 </h7> 
+                <br/>
+                
+                <h6>Email: </h6><h7> kxtemas01@gmail.com </h7>
+                <br/>
+  
+                <h6>Linkedln: </h6>
+             <a href="https://www.linkedin.com/in/katiemunoz01" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/linkedin.svg" width="32" height="32" /></a>   /Katiemunoz01
+            
+              </div>
 
-          <div className="c-desc">
-
-
-            <p className="c-right">
-
-            </p>
-            <p className="c-descs">
-              <b>Education</b>
-              <p></p>
-              Bachelor of Science in Software Development  <br></br>
-              Grand Canyon University, Arizona, USA <br></br>
-              Expected Graduation Date: April 2023  <br></br>
-
+              <div class="rc" data-title="Career Objectives">
+           <br/>
+            Katie is a Senior at Grand Canyon University working towards a Bachelor's Degree in Software Development. She is a dedicated student with a passion for growing her knowledge in the software development field. 
+            This field of study has provided greater experience in Object Oriented Programming and Java, as well as extensive experience with PHP and C# programming languages. Within these languages, Katie has experience with HTML, CSS, and JavaScript, particularly AJAX, for front end development, MVC design patterns within the Laravel and ASP.NET Core frameworks, and MySQL database design and manipulation. She is a quick learner with a passion for creating quality work.
+          </div>
+          
+          <div class="rc" data-title="Education">
+            <br/>
+          <div class="bold">
+              Bachelor of Science in Software Development  <br/>
+              Grand Canyon University, Arizona, USA    <br/>
+              Expected Graduation Date: April 2023     <br/>
+              </div>
               Pursuing a bachelor's degree in Software Development with a focus on developing in-depth knowledge and skills in software design, development, and testing.
               Maintaining a 3.7 GPA, demonstrating a strong academic record and a commitment to academic excellence.
               Participated in several software development projects, including designing, coding, and testing applications using various programming languages and tools.
               Completed coursework in advanced topics such as data structures, algorithms, software design patterns, databases, and web development.
-              Participated in team-based projects, developing strong collaboration and communication skills and a solid understanding of software development methodologies.
-            </p>
-            <p className="c-descs">
-              <b>Work Experience</b>
-              <p></p>
-              MEMBER SALES REPRESENTATIVE, Surprise, AZ <br></br>
-              United PF, LLC, April 2021-Present <br></br>
-              Followed company procedures to count and manage funds received from customers.
-              Offered best-in-class service to members through prompt, courteous and professional interactions. Provided excellent customer service by assisting, responding to and resolving customer inquiries. Trained staff on office procedures and software applications, driving operations.
-              Assisted with new hire orientation and employee training.
-              Troubleshot minor problems and reported larger technical issues.
-              <p></p>
-              SHIFT MANAGER, Surprise, AZ <br></br>
-              AGK Restaurants, February 2019-March 2021<br></br>
-              Completed incident reports needed to document damage, theft or personal injury claims.
-              Tracked store inventories and replenished item levels by ordering new products.
-              Tracked employee progress and implemented corrective actions to minimize mistakes and promote growth. Created staff teams and delegated work tasks to increase productivity.
-              Promoted customer satisfaction by addressing complaints and issuing refunds.
-              Set employee schedules and made staffing adjustments to get shifts covered.
-              Balanced cash drawers and resolved financial discrepancies using receipts and financial document
-            </p>
+              Participated in team-based projects, developing strong collaboration and communication skills and a solid understanding of software development methodologies
+            </div>
+            <br/>
+            <div class="rc" data-title="Additional Skills">
+           <br/>
+              <li>Bilingual: Spanish Speaking</li> 
+              <li> Problem-Solving</li> 
+              <li>Adminstrative</li>
+              <li> Teamwork</li>
+              <li>Communication</li>
+           
+            </div>
+           
+           
           </div>
         </div>
       </div>
-    </div>
+
 
 
   );
